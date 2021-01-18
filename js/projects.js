7b0a9bad2f13ba43cd28ee39c4c3b899bfc83f2d
Alex <nishtidight@ukr.net>
2021-01-18 18:20:40 +0200

    pie chart appended

--- js/projects.js
+++ js/projects.js
@@ -1,42 +1,68 @@
 const projects = [
-    {projName: {
-        en: "Fashion Shop Boutique",
-        ru : "Fashion Shop Boutique",
-        ua : "Fashion Shop Boutique"
+    {   
+        projName: {
+            en: "Fashion Shop Boutique",
+            ru : "Fashion Shop Boutique",
+            ua : "Fashion Shop Boutique"
+        },
+        projType: {
+            en: "Landing Page",
+            ru: "лендинг",
+            ua: "лендінг"
+        },
+        projLogo: "img/projects/fashionshop.jpg",
+        projLink: "projects/fashionshop/index.html",
+        projDescr: {
+            en: "This is a landing page for internet store. This page has responsive desigh and includes several sliders and countdown timer. In mobile mode menu dropdown and accordion are implemented. Production version is optimized. WEBP picture format is included.",
+            ru: "Этот проект - лендинг для рекламной акции интернет магазина. Страница адаптивная, включает в себя несколько слайдеров и таймер обратного отсчёта. В мобильной версии добавлены выпадающее меню и аккордеон. Продакшн версия оптимизирована. Добавлен формат изображений WEBP.",
+            ua: "Ця сторінка - лендінг для рекламної акції інтернет магазину. Сторінка адаптивна, включає в себе декілька слайдерів і таймер зворотнього відліку. у мобільній версії додані випадаюче меню та акордион. Сторінка оптимізована. Додан формат зображень WEBP."
+        },
+        usedTechs: ["html5","css","sass","jquery","slick","gulp"],
+        codeLink: "https://github.com/Oleksii-Tkachenko/Fashionshop"
     },
-    projType: {
-        en: "Landing Page",
-        ru: "лендинг",
-        ua: "лендінг"
-    },
-    projLogo: "img/projects/fashionshop.jpg",
-    projLink: "projects/fashionshop/index.html",
-    projDescr: {
-        en: "This is a landing page for internet store. This page has responsive desigh and includes several sliders and countdown timer. In mobile mode menu dropdown and accordion are implemented. Production version is optimized. WEBP picture format is included.",
-        ru: "Этот проект - лендинг для рекламной акции интернет магазина. Страница адаптивная, включает в себя несколько слайдеров и таймер обратного отсчёта. В мобильной версии добавлены выпадающее меню и аккордеон. Продакшн версия оптимизирована. Добавлен формат изображений WEBP.",
-        ua: "Ця сторінка - лендінг для рекламної акції інтернет магазину. Сторінка адаптивна, включає в себе декілька слайдерів і таймер зворотнього відліку. у мобільній версії додані випадаюче меню та акордион. Сторінка оптимізована. Додан формат зображень WEBP."
-    },
-    usedTechs: ["html5","css","sass","jquery","slick","gulp"],
-    codeLink: "https://github.com/Oleksii-Tkachenko/Fashionshop"
+    {   
+        projName: {
+            en : "Restaurant menu",
+            ru : "Меню ресторана",
+            ua : "Ресторанне меню"
+        },
+        projType: {
+            en: "React app",
+            ru: "React app",
+            ua: "React app"
+        },
+        projLogo:"img/projects/restmenu.jpg",
+        projLink: "projects/restmenu/index.html",
+        projDescr: {
+            en: "This is a training project which deals with React app development. It includes restaurant menu layout. Dishes are received from json server. There is a possibility to filter dishes by category and calculate total bill amount.",
+            ru: "Это учебный проект по разработке на React. Он включает ресторанное меню с возможностью фильтрации блюд по категориям и получения общего счёта. Блюда загружаются с JSON сервера.",
+            ua: "Це учбовий проект по розробці на React. Він включає ресторанне меню з можливістю фільтрації страв по категоріях та отримання суми замовлення. Страви завантажуються з JSON сервера."
+        },
+        usedTechs: ["js", "react", "redux", "sass"],
+        codeLink: "https://github.com/Oleksii-Tkachenko/Restaurant-Menu"
     },
 
-    {projName: {
-        en : "another project",
-        ru : "другой проект",
-        ua : "інший проект"
-    },
-    projType: {
-        en: "Module",
-        ru: "модуль",
-        ua: "модуль"
-    },
-    projLogo:"img/icons/css.png",
-    projDescr: {
-        en: "This is pet project",
-        ru: "",
-        ua: ""
-    },
-    usedTechs: ["git", "js", "webpack"]}
+    {
+        projName: {
+            en: "Pie chart constructor",
+            ru: "Конструктор круговых диаграмм",
+            ua: "Конструктор секторних діаграм"
+        },
+        projType: {
+            en: "Module",
+            ru: "Модуль",
+            ua: "Модуль"
+        },
+        projLogo: "img/projects/Pie-chart.jpg",
+        projLink: "https://codepen.io/AlexeiTk/pen/yLJMEVK",
+        projDescr: {
+            en: "This is an interactive pie chart constructor. It based on SVG technology especially on <animate> tag. With this module you may construct pie chart you like and name it.",
+            ru: "Этот модуль представляет собой интерактивный конструктор круговых диаграмм. Он базируется на технологии SVG. В особенности, применён тег <animate>. Этот модуль позволяет конструировать и называть круговые диаграммы.",
+            ua: "Цей модуль являє собою інтерактивний конструктор секторних діаграм. Він базується на технології SVG. Зокрема, тег <animate> примінений для інтерактивості. Цей модуль дозволяє конструювати і називати секторні діаграми."
+        },
+        usedTechs: ["html5", "sass", "js", "svg"],
+        codeLink: "https://codepen.io/AlexeiTk/pen/yLJMEVK"
+    }
 ];
 
 export default projects;
